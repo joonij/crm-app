6404e3a49c4fb7184e33847d7eea860f46cdee75
junhee <joonij93@gmail.com>
2026-06-28 16:30:40 +0900

    fix: Vercel 에러 해결을 위한 경로 재수정 및 강제 푸시

--- app/clients/[id]/page.tsx
+++ app/clients/[id]/page.tsx
@@ -7,7 +7,7 @@ import { ChevronLeft, FileText, Stethoscope, Calendar, User, Crown } from "lucid
 import { supabase } from "@/lib/supabase";
 import ClientDetailModal from "./components/ClientDetailModal";
 import ClientMemoCard from "./components/ClientMemoCard";
-import ClientCoverageCard from "./components/ClientCoverageCard";
+import ClientCoverageCard from "./components/ClientCoverageCard"; // 깃허브 강제 업데이트
 import ClientsMedicalHistoryCard from "./components/ClientsMedicalHistoryCard";
 import ClientScheduleCard from "./components/ClientScheduleCard";
 
