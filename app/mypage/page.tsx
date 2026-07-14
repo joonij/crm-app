3d3bd39c9ad60f75ae376d7690cc170f46d542b8
정준희 <joonij93@gmail.com>
2026-07-15 01:43:34 +0900

    명함 페이지

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -235,7 +235,7 @@ export default function MyPage() {
       kakao.Share.sendDefault({
         objectType: 'feed',
         content: {
-          title: `[디지털 명함] ${profile?.corporation_name}\n${profile?.name} ${profile?.rank}`,
+          title: `[디지털 명함] ${profile?.corporation_name}\n${profile?.name} ${profile?.rank}\n${profile?.phone}`,
           description: profile?.bio || "고객님의 든든한 금융 파트너가 되겠습니다.",
           imageUrl: profile?.avatar_url || defaultImageUrl,
           link: {
